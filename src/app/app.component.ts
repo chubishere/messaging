d6f6e949a73c2e45ae235cac617620db678d52cc
Clarence Lee <clarencedglee@gmail.com>
2017-02-15 21:03:47 +0000

    adding x2js but ts shows error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import * as X2JS from 'x2js';
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent { 
+	xml = `
+		<notes attr="atVal">
+			<note>A</note>
+			<note>B</note>
+		</notes>`;
+	name = 'Angular' + this.process();
+
+	process(){
+		let xj = new X2JS();
+		console.log(xj.xml2js(this.xml));
+		return '';
+	}
+}
 
